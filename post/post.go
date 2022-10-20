bd1e4d88469cf89854363e7ac124593bf87f33b7
Lariskovski <larissaporto@live.com>
2022-10-19 21:44:03 -0300

    Add POST method to lambda

---
+++ post/post.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Pokemon struct {
+	Name string `json:"name"`
+	Types []string `json:"types"`
+	Image string `json:"image"`
+	Ability string `json:"ability"`
+	BaseStats map[string]string `json:"baseStats"`
+}
+
+
+func main() {
+	lambda.Start(postPokemon)
+}
+
+
+// Returns all pokemons if no query string requested
+func postPokemon(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	mongoURI := os.Getenv("MONGODB_URI")
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		log.Fatal(err)
+	}
+	Context := context.Background()
+	err = client.Connect(Context)
+	if err != nil {
+		log.Fatal(err)
+	}
+	PokemonsCollection := client.Database("pokedex").Collection("pokemon")
+	
+	
+	var pokemon Pokemon
+	json.Unmarshal([]byte(request.Body), &pokemon)
+
+	_, err = PokemonsCollection.InsertOne(Context, pokemon)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return events.APIGatewayProxyResponse{StatusCode: 201}, nil
+}
+
